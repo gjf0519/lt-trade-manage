f762a2c8bcb23b8b9102b76548dca073275310db
DEC123123 <jianfeng.gai@pharmacodia.com>
2020-04-08 21:13:57 +0800

    登录认证

--- lt-trade-task/src/main/java/com/lt/task/FundDetailThread.java
+++ lt-trade-task/src/main/java/com/lt/task/FundDetailThread.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.*;
